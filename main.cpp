465de790e66ae7d50b20165aa21a46f9d9714740
zhangfengup <49608725+zhangfengup@users.noreply.github.com>
2019-06-15 15:19:09 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char** argv) {
+int main() {
 	printf("hello");
 	return 0;
 }
